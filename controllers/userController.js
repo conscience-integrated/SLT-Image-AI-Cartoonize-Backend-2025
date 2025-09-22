e64d6b52ec89f7537f817e5bc6dd394f540087ba
shaluka samaranayaka <shaluws95@gmail.com>
2025-09-22 12:43:34 +0530

    change user create function issue.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,17 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
 exports.createUser = (req, res) => {
   const { name, mobile } = req.body;
-  
+
   User.create({ name, mobile }, (err, results) => {
     if (err) {
-      return res.status(500).json({ error: 'Database error' });
+      return res.status(500).json({ error: "Database error" });
+    } else {
+      res.json({
+        success: true,
+        userId: results.insertId,
+        message: "User created successfully",
+      });
     }
-    
-    res.json({ 
-      success: true, 
-      userId: results.insertId,
-      message: 'User created successfully' 
-    });
   });
 };
